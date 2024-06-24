30a5dfe518b8c2e85c970aa3f8d125ab34004d17
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-06-24 16:40:28 +0200

    [UPD] ADDED A GENERATOR AND PRINT PASSWORD

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php 
 
 $password_length = $_GET['password_length'];
+$upper_chars = array("A","B","C","D","E","F","G","H","J","K","M","N","P","Q","R","S","T","U","V","W","X","Y","Z");
+$lower_chars = array("a","b","c","d","e","f","g","h","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z");
+$numbers = array("2","3","4","5","6","7","8","9");
+$symbols = array("!","#","$","%","&","*","+","-","?","@");
+
+$array_password= array_merge($upper_chars,$lower_chars,$numbers,$symbols);
+
+
+    
+for($i=0; $i <= $password_length; $i++){
+    $randomIndex = rand(0, 64);
+    $password = $password . $array_password[$randomIndex];
+
+};
+
+
+
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -16,6 +36,10 @@ $password_length = $_GET['password_length'];
         <button type="submit">Genera</button>
 
     </form>
+    
     <?php echo "lunghezza password $password_length " ?>
+    <hr>
+    <?php echo $password;?>
+
 </body>
 </html>
