1a03a4f8fb1d32ec9ca05d09745ab0c39c807890
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-06-24 16:44:16 +0200

    SPLIT LOGIC AND LAYOUT

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php 
 
-$password_length = $_GET['password_length'];
-$upper_chars = array("A","B","C","D","E","F","G","H","J","K","M","N","P","Q","R","S","T","U","V","W","X","Y","Z");
-$lower_chars = array("a","b","c","d","e","f","g","h","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z");
-$numbers = array("2","3","4","5","6","7","8","9");
-$symbols = array("!","#","$","%","&","*","+","-","?","@");
-
-$array_password= array_merge($upper_chars,$lower_chars,$numbers,$symbols);
-
-
-    
-for($i=0; $i <= $password_length; $i++){
-    $randomIndex = rand(0, 64);
-    $password = $password . $array_password[$randomIndex];
-
-};
-
-
+include 'logic.php';
 
 
 
@@ -37,7 +21,7 @@ for($i=0; $i <= $password_length; $i++){
 
     </form>
     
-    <?php echo "lunghezza password $password_length " ?>
+   
     <hr>
     <?php echo $password;?>
 
